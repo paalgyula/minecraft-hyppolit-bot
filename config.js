3caf26064ad7072b67df55f07ca751d62c35a230
Paál Gyula <paalgyula@paalgyula.com>
2018-02-21 13:02:50 +0100

    Default config file commited

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+    host: 'dev2.paalgyula.com', // Hostname
+    port: 25565, // Port
+    username: 'Minion', // Login name
+    password: '********' // Password (for /login command)
+};
+
+module.exports = config;
